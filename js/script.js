8517cf0bd02a6ec670810f3cb2fca2b0dd2505dc
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-22 12:43:23 +0530

    animations

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-const navlogo = document.querySelector('#nav-logo');
+// Logo Change
+const navlogo = document.querySelector("#nav-logo");
 
-window.addEventListener("scroll", function(){
-    const scrollHeight = window.pageYOffset
-    if(scrollHeight > 600){
-        navlogo.src = "assets/images/logo-black.svg";
-    }else{
-        navlogo.src = "assets/images/logo.svg";
-    }
-})
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > 600) {
+    navlogo.src = "assets/images/logo-black.svg";
+  } else {
+    navlogo.src = "assets/images/logo.svg";
+  }
+});
